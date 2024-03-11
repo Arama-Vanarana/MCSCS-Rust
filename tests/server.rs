a28c3397b68779bfd245ee86fa45b0a7c0b6d997
Arama <3584075812@qq.com>
2024-03-12 07:46:49 +0800

    🧪 test(tests): 添加servers测试

---
+++ tests/server.rs
@@ -0,0 +1,15 @@
+use mcscs::{pages::{choose_server, init}, server::load_servers_lists};
+
+#[tokio::test]
+async fn test_get_server_config() {
+    init::main().await.expect("main()");
+    let data = load_servers_lists(Some(
+        choose_server("需要获取")["name"]
+            .as_str()
+            .expect("test_get_server_config()"),
+    ));
+    println!(
+        "data: {}",
+        serde_json::to_string_pretty(&data).expect("test_get_server_config()")
+    );
+}
